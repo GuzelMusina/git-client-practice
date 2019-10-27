212411d00b8afeacaa95f30f9795879cecca2321
GuzelMusina <guzmus@rambler.ru>
2019-10-27 23:09:18 +0300

    add simple react redux app

--- src/App.js
+++ src/App.js
@@ -1,8 +1,13 @@
-import React from 'react';
-import './App.css';
+import React, {Component} from 'react';
+import HelloWorld from "./HelloWorld";
+import {createStore} from "redux";
+import reducer from "./reducers"
+const initialState = { tech: "React " };
+const store = createStore(reducer, initialState);
 
-function App() {
-    return <div><h1>Hello Guzel!</h1></div>
+class App extends Component{
+    render() {
+        return<HelloWorld tech={store.getState().tech}/>
+    }
 }
-
 export default App;
