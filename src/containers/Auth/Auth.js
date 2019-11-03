6ca35866f77126caacf79d01702570ae337bddd0
GuzelMusina <guzmus@rambler.ru>
2019-11-03 17:58:29 +0300

    create Auth page and Button component without style

---
+++ src/containers/Auth/Auth.js
@@ -0,0 +1,36 @@
+import React, {Component} from "react";
+import classes from './Auth.css'
+import Button from "../../components/UI/Button/Button";
+export default class Auth extends Component{
+
+    loginHandler=()=>{
+
+    }
+
+    registerHandler=()=>{
+
+    }
+    submitHandler=event=>{
+        event.preventDefault();
+    }
+
+
+    render(){
+        return(
+            <div className={classes.Auth}>
+                <div>
+                    <h1>Authorization</h1>
+                    <form onSubmit={this.submitHandler}>
+                        <input type="text"/>
+                        <input type="text "/>
+
+                        <Button type="success" onClick={this.loginHandler}>Войти</Button>
+                        <Button type="primary" onClick={this.registerHandler}>Зарегестрироваться</Button>
+
+                    </form>
+                </div>
+
+            </div>
+        )
+    }
+}
