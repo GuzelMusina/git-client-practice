69961b41d70caa793084a6d65e3f21de239e9e13
GuzelMusina <guzmus@rambler.ru>
2019-11-18 21:04:12 +0300

    add auth

---
+++ src/components/app/Auth.js
@@ -0,0 +1,108 @@
+import React from "react";
+import {
+    BrowserRouter as Router,
+    Switch,
+    Route,
+    Link,
+    Redirect,
+    useHistory,
+    useLocation
+} from "react-router-dom";
+import Profile from "./Profile";
+
+export default function Auth() {
+    return (
+        <Router>
+            <div>
+                <AuthButton/>
+                <ul>
+                    <li>
+                        <Link to={"/login"}>Login</Link>
+                    </li>
+                    <li>
+                        <Link to={"/profile"}>Go to my profile</Link>
+                    </li>
+                </ul>
+                <Switch>
+                    <Route path="/login">
+                        <LoginPage/>
+                    </Route>
+                    <PrivateRoute path="/profile">
+                        <Profile/>
+                    </PrivateRoute>
+                </Switch>
+            </div>
+        </Router>
+    );
+}
+
+const fakeAuth = {
+    isAuthenticated: false,
+    authenticate(cb) {
+        fakeAuth.isAuthenticated = true;
+        setTimeout(cb, 100); // fake async
+    },
+    signout(cb) {
+        fakeAuth.isAuthenticated = false;
+        setTimeout(cb, 100);
+    }
+};
+
+function AuthButton() {
+    let history = useHistory();
+
+    return fakeAuth.isAuthenticated ? (
+        <p>
+            Welcome!{" "}
+            <button
+                onClick={() => {
+                    fakeAuth.signout(() => history.push("/"));
+                }}
+            >
+                Sign out
+            </button>
+        </p>
+    ) : (
+        <p>You are not logged in.</p>
+    );
+}
+
+function PrivateRoute({children, ...rest}) {
+    return (
+        <Route
+            {...rest}
+            render={({location}) =>
+                fakeAuth.isAuthenticated ? (
+                    children
+                ) : (
+                    <Redirect
+                        to={{
+                            pathname: "/profile",
+                            state: {from: location}
+                        }}
+                    />
+                )
+            }
+        />
+    );
+}
+
+
+function LoginPage() {
+    let history = useHistory();
+    let location = useLocation();
+
+    let {from} = location.state || {from: {pathname: "/"}};
+    let login = () => {
+        fakeAuth.authenticate(() => {
+            history.replace(from);
+        });
+    };
+
+    return (
+        <div>
+            <p>You must log in to view the page at {from.pathname}</p>
+            <button onClick={login}>Log in</button>
+        </div>
+    );
+}
