ea0eeba3eaa41fe5329186a04c814cf1eb94a698
GuzelMusina <guzmus@rambler.ru>
2019-12-16 15:58:30 +0300

    add routes

--- src/index.js
+++ src/index.js
@@ -1,17 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { ApolloProvider } from 'react-apollo';
-import { ApolloClient } from 'apollo-client';
-import { HttpLink } from 'apollo-link-http';
-import { InMemoryCache } from 'apollo-cache-inmemory';
+import {ApolloProvider} from 'react-apollo';
+import {ApolloClient} from 'apollo-client';
+import {HttpLink} from 'apollo-link-http';
+import {InMemoryCache} from 'apollo-cache-inmemory';
 import './index.css'
 import 'typeface-roboto';
-import history from "./history";
-import { Router, Route, Link } from 'react-router-dom';
-
+import {Router, Route, Link, Redirect, BrowserRouter} from 'react-router-dom';
 import App from './components/app/main/App';
-
 import registerServiceWorker from './registerServiceWorker';
+import {Switch} from "@material-ui/core";
+import RepositoryList from "./components/app/repositories/RepositoriesList";
+import Profile from "./components/app/profile/Profile";
+import {Search} from "./components/app/search/Search";
+import {setContext} from "apollo-link-context";
+import Auth from "./components/UI/Auth";
 
 const cache = new InMemoryCache();
 
@@ -25,7 +28,16 @@ const httpLink = new HttpLink({
         }`,
     },
 });
-
+const authLink = setContext((_, {headers}) => {
+    const token = localStorage.getItem('token');
+    return {
+        headers: {
+            ...headers,
+            authorization:
+                token ? `Bearer ${token}` : ''
+        }
+    }
+});
 const client = new ApolloClient({
     link: httpLink,
     cache,
@@ -33,9 +45,22 @@ const client = new ApolloClient({
 
 ReactDOM.render(
     <ApolloProvider client={client}>
-        <App />
+        <BrowserRouter>
+            <Redirect from="/" to="/login"/>
+            <App>
+                <Switch>
+                    <Route exact path='/login' component={Auth}/>
+                    <Route path='/repository/:login/:name' component={RepositoryList}/>
+                    <Route path='/profile/:login' component={Profile}/>
+                    <Route path='/my-profile' component={Profile}/>
+                    <Route path='/search' component={Search}/>
+                </Switch>
+            </App>
+        </BrowserRouter>
     </ApolloProvider>,
     document.getElementById('root'),
 );
 
 registerServiceWorker();
+
+export default client;
